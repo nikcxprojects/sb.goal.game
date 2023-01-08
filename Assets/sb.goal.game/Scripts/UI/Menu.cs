594e9317567af8abc7a7e61ac58db7148f32cf71
Sladkiy Eduard <nikcxprojects@gmail.com>
2023-01-08 16:08:29 +0300

    update

--- Assets/sb.goal.game/Scripts/UI/Menu.cs
+++ Assets/sb.goal.game/Scripts/UI/Menu.cs
@@ -3,47 +3,28 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField] Button goldenBoots;
-    [SerializeField] Button bestPenalty;
-    [SerializeField] Button speedyMaster;
-
-    [Space(10)]
-    [SerializeField] Button footballRulesBtn;
-    [SerializeField] Button bestPlayersBtn;
-
-    [Space(10)]
-    [SerializeField] GameObject VFX;
+    [SerializeField] Button footballExpert;
+    [SerializeField] Button flyBall;
+    [SerializeField] Button guessChampion;
 
     private void Start()
     {
-        VFX.SetActive(true);
-
-        goldenBoots.onClick.AddListener(() =>
+        footballExpert.onClick.AddListener(() =>
         {
             UIManager.OpenWindow(Window.GoldenBootsMenu, gameObject);
             AppManager.CurrentGameType = GameType.GB;
         });
 
-        bestPenalty.onClick.AddListener(() =>
+        flyBall.onClick.AddListener(() =>
         {
             UIManager.OpenWindow(Window.BestPenaltyMenu, gameObject);
             AppManager.CurrentGameType = GameType.BP;
         });
 
-        speedyMaster.onClick.AddListener(() =>
+        guessChampion.onClick.AddListener(() =>
         {
             UIManager.OpenWindow(Window.SpeedyMasterMenu, gameObject);
             AppManager.CurrentGameType = GameType.SM;
-        });
-
-        footballRulesBtn.onClick.AddListener(() =>
-        {
-            UIManager.OpenWindow(Window.FootballRules, gameObject);
-        });
-
-        bestPlayersBtn.onClick.AddListener(() =>
-        {
-            UIManager.OpenWindow(Window.BestPlayers, gameObject);
         });
     }
 }
