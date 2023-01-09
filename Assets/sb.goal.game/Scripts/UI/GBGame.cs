405fc678595d581f26e9695ece9a763d545d0f51
Sladkiy Eduard <nikcxprojects@gmail.com>
2023-01-09 16:55:36 +0300

    update

--- Assets/sb.goal.game/Scripts/UI/GBGame.cs
+++ Assets/sb.goal.game/Scripts/UI/GBGame.cs
@@ -3,5 +3,26 @@ using UnityEngine.UI;
 
 public class GBGame : MonoBehaviour
 {
-    
+    private int score;
+
+    [SerializeField] Button pauseBtn;
+    [SerializeField] Button settingsBtn;
+
+    [Space(10)]
+    [SerializeField] Text scoreText;
+
+    private void Start()
+    {
+        ScoreUtility.CurrentScore = score;
+
+        pauseBtn.onClick.AddListener(() =>
+        {
+            UIManager.OpenWindow(Window.Pause);
+        });
+
+        settingsBtn.onClick.AddListener(() =>
+        {
+            UIManager.OpenWindow(Window.Settings);
+        });
+    }
 }
